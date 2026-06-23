81a5928c1057cd6349c9a6182d5365492d59ebeb
nolan <272226345+NolanJX@users.noreply.github.com>
2026-06-23 23:12:01 +0800

    chore(fish): add c abbr for clear

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
   set -x no_proxy 'localhost,127.0.0.1/8,::1'
   set -x NO_PROXY 'localhost,127.0.0.1/8,::1'
 
+  abbr --add c clear
+
   # Git
   abbr --add g git
   abbr --add gst git status
