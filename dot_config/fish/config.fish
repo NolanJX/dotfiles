1a7158ee2706370a1b7f4b0e9c8b1ff1b4cf3e49
nolan <272226345+NolanJX@users.noreply.github.com>
2026-08-17 12:40:08 +0800

    chore(fish): add glt abbr

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,6 +8,7 @@ if status is-interactive
   abbr --add g git
   abbr --add gst git status
   abbr --add gco git checkout
+  abbr --add glt 'git log --pretty="format:%C(auto,yellow)%h %C(auto,cyan)%ai %C(auto,green)%ci %Creset%s"'
 
   # Lazygit
   # abbr --add lg lazygit
